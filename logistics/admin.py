f3d0aca074ba944ab3f776fffa3d100edfeda4a9
acm-97 <alejandro97.acm@gmail.com>
2025-04-09 10:35:33 -0400

    [update] admin view

--- logistics/admin.py
+++ logistics/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Booking, Vehicle
 
-# Register your models here.
-admin.site.register(Booking)
-admin.site.register(Vehicle)
+class VehicleInline(admin.TabularInline):
+    model = Vehicle
+    extra = 3
+
+
+class BookingAdmin(admin.ModelAdmin):
+    inlines = [VehicleInline]
+
+
+admin.site.register(Booking, BookingAdmin)
